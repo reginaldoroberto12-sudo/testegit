276e84aba6b3049ef8e24e8b3da4196c56f1a7db
REGINALDO ROBERTO MIRANDA PONTES <reginaldo.rmpontes@senacsp.edu.br>
2026-02-10 20:33:05 -0300

    10/01 -app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log('Olá, mundo!  Este é o meu primeiro programa em JavaScript. Estou aprendendo a programar e estou muito animado para criar coisas incríveis com código!');
+
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+//somar dois números
+ function somar(a, b) {
+     return a + b;
+ }
+// apresentar o resultado da soma
+ const resultado = somar(5, 3);
+ console.log('O resultado da soma é:', resultado);
