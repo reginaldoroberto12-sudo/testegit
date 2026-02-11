57e43d31189f8ce614b96e03a41e0aed93256c15
REGINALDO ROBERTO MIRANDA PONTES <reginaldo.rmpontes@senacsp.edu.br>
2026-02-10 22:08:09 -0300

    nova alteração no codigo app.js

--- app.js
+++ app.js
@@ -5,3 +5,5 @@ console.log('Olá, Mundo! Este é o meu primeiro programa em Node.js. Estou anim
     // const port = 3000;  
     
     const variavel = document.getElementById("root").innerHTML= "Meu primeiro codigo em JavaScript. Ja sou programador Júnior!";
+
+    console.log("meu nome é Reginaldo, tenho 35 anos, sou casado e tenho 2 filhos. Sou programador Júnior e estou animado para aprender mais sobre desenvolvimento de software e criar aplicações incríveis!");
